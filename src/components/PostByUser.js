a2744db853af70879ea61a185174146c91c063a5
Linda Wang <lindawang256@gmail.com>
2019-03-17 09:19:07 -0700

    Add filter by userId feature

--- src/components/PostByUser.js
+++ src/components/PostByUser.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PostItem from './PostItem';
 
-const PostByUser = (props)=>{
-    return(
-        <div>All the posts by the user you Selected</div>
-    )
+class PostByUser extends React.Component{
+    getUserPosts=()=>{
+        return this.props.postsOfSelectedUser.map(post=>{
+            return (
+                <div key={post.id}>
+                    <PostItem post={post} handleSelectUser={this.props.handleSelectUser}/>
+                </div>
+            )
+        })
+    }
+    
+    render(){
+        return(
+            <div>
+                All the posts by the user you Selected
+                {this.getUserPosts()}
+            </div>
+        )
+    }
+    
 }
 
 export default PostByUser;
